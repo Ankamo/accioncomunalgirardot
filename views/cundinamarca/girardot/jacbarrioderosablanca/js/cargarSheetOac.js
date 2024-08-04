f08ee98913f3c26ae0a6dd651a1776d01ccf233d
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-08-04 00:25:37 -0500

    w

--- views/cundinamarca/girardot/jacbarrioderosablanca/js/cargarSheetOac.js
+++ views/cundinamarca/girardot/jacbarrioderosablanca/js/cargarSheetOac.js
@@ -1,4 +1,1 @@
-// public/js/config.js
-
-// IDs de las hojas de cálculo
 var Oac = '1pym0y9ZL2v5-6Xt43DeH5B3vxYJWlGFQj6UJ42KYt24';
--- views/cundinamarca/girardot/oac/js/cargarSheetOac.js
+++ views/cundinamarca/girardot/oac/js/cargarSheetOac.js
@@ -1,4 +1,1 @@
-// public/js/config.js
-
-// IDs de las hojas de cálculo
 var Oac = '1dJRtbDiIJFAbaMEw50kjdSHOO84kWZt-wXLZow_ILWo';
