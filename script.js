eaa52a3eab8ede01d4aae9b27148b64ae69fa58f
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-08-28 19:48:48 -0500

    sda

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const greeting = document.querySelector("#inicio h1");
-    greeting.addEventListener("click", function() {
-        alert("¡Gracias por visitar nuestro sitio!");
-    });
-});
