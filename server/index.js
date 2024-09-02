aaf0a0a4616ba3211c6251995dbeb538cf84ced6
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-09-02 00:53:31 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express ();
 
 app.use("/", (req,res) => {
-    res.send ("Servidor funcional Ok");
+    res.send ("Servidor funcional");
 });
 
 app.listen(5000, console.log("Servidor funcinal en el puerto 5000"));
