4eceb515d2e3a841b731e98cfb710ae31bdd94a9
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-09-03 13:31:07 -0500

    dca

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,25 +1,10 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
-import Head from '@/components/head';  // Importa el componente Head
-import Header from '@/components/header';
-import Nav from '@/components/nav';
-import Footer from '@/components/footer';
 import '@/styles/globals.css'; // Importa los estilos globales
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
-            {/* Configuración del encabezado global */}
-            <Head title="Default Title" />
-            {/* Encabezado y navegación global */}
-            <Header />
-            <Nav />
-            <main className="main">
-                <Component {...pageProps} />
-            </main>
-            {/* Pie de página global */}
-            <Footer />
-        </>
+        <Component {...pageProps} />
     );
 }
 
