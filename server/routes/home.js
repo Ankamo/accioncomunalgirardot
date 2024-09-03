dc0315541b129e77875493ecc81112e74942d139
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-09-03 16:25:06 -0500

    dsa

--- server/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/home", (req, res) => {
-    res.json({ message: "Like this video!", people: ["Arpan", "Jack", "Barry"] });
-});
-
-module.exports = router;
