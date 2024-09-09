54c487b1d95362cc95f56a651a87e28d452dc8b8
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-09-09 00:30:02 -0500

    dax

---
+++ web/src/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer>
+            {/* Contenido del pie de página */}
+            <p>&copy; 2024 Junta de Acción Comunal - Rosa Blanca</p>
+        </footer>
+    );
+}
---
+++ web/src/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            {/* Contenido del encabezado */}
+            <h1>Junta de Acción Comunal</h1>
+        </header>
+    );
+}
