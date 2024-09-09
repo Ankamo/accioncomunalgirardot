4adbc36039f307cd036a2e49dd669513cdda3d3a
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-09-09 01:42:02 -0500

    scas

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h2>Bienvenido a la Junta de Acción Comunal del Barrio Rosa Blanca</h2>
+      <h2>s</h2>
       {/* Contenido específico de la página de inicio */}
     </div>
   );
--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,8 +1,25 @@
+import styles from "../styles/footer.module.css"; // Asegúrate de que la ruta es correcta
+
 export default function Footer() {
     return (
-        <footer>
-            {/* Contenido del pie de página */}
-            <p>&copy; 2024 Junta de Acción Comunal - Rosa Blanca</p>
+        <footer className={styles.footer}>
+            <div className={styles.info}>
+                <p className={styles.p}>Diagonal 33 con Carrera 12 A esquina.</p>
+                <p className={styles.p}>Barrio: Rosa Blanca Primer Sector</p>
+                <p className={styles.p}>juntacomunalrosablanca@gmail.com</p>
+                <p className={styles.p}>(+57) 304 347 0984</p>
+            </div>
+            <div className={styles.logos}>
+                <img src="" alt="Logo Footer Izquierdo" className={styles.img} />
+                <img src="" alt="Logo Footer Centro 1" className={styles.img} />
+                <img src="" alt="Logo Footer Centro 2" className={styles.img} />
+                <img src="" alt="Logo Footer Derecho" className={styles.img} />
+                
+            </div>
+            <div className={styles.propiedad}>
+                <p className={styles.p}>Periodo: 2022-2026</p>
+                <p className={styles.p}>Creado y Diseñado Por: Ankamo</p>
+            </div>
         </footer>
     );
 }
