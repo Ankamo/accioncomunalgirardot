0dba20afa1062338c3d696e6c01566ad6a13a527
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-09-11 23:23:42 -0500

    sx

--- web/server/server.js
+++ web/server/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 8080;
 
+
+app.use(cors());
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello World!" });
+    res.json({ message: "Hola a Todos!" });
 });
 
 app.listen(PORT, () => {
