a0b58fbd57755d22fc0fee9acb5d803edac8590e
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-09-11 23:24:09 -0500

    sx

--- web/server/server.js
+++ web/server/server.js
@@ -3,13 +3,12 @@ const app = express();
 const cors = require("cors");
 const PORT = 8080;
 
-
 app.use(cors());
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hola a Todos!" });
+app.get("/api/home", (req, res) => {
+  res.json({ message: "Like this video!", people: ["Arpan", "Jack", "Barry"] });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
